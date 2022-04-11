9f15ce0d0a06e8a60795258a8a5bfb5d9cc59646
avivilloz <avivilloz@gmail.com>
2022-04-11 09:20:05 +0300

    ur

--- interview_preparation_liveu/thread_pool.py
+++ interview_preparation_liveu/thread_pool.py
@@ -1,51 +1,53 @@
-from threading import Thread, Lock
+from threading import Thread, Lock, Condition
+import time
 
 # lock.acquire(), lock.release()
 
 class ThreadPool:
     def initial_setup(self, num_of_workers):
-        self.workers = [self.Worker(self)] * num_of_workers
+        self.workers = [self.Worker(self, cond)] * num_of_workers
 
     def do(self, task, args):
         task_delivered = False
         while not task_delivered:
-            self.mutex.acquire()
+            # self.mutex.acquire()
             if self.workers:
                 worker = self.workers[0]
                 self.workers.pop(0)
                 worker.task = task
                 worker.args = args
-                worker.start()
+                worker.has_task = True
+                self.cond.notify()
             else:
                 print("all workers busy")
-            self.mutex.release()
+            self.cond.wait()
+            # self.mutex.release()
 
-
-    def __get_back_to_line__(self, worker):
-        self.mutex.acquire()
-        self.workers = [worker] + self.workers
-        self.mutex.release()
+    def get_task(self):
+        self.cond.wait()
+        return self.tasks[0]
 
     workers = []
-    mutex = Lock
+    tasks = []
+    cond = Condition()
 
     class Worker:
-        def work(self, pool):
-            self.task(self.args)
-            pool.__get_back_to_line__(pool, self)
+        def __work__(self, pool, cond):
+            while self.should_work:
+                task = pool.get_task()
+                task[0](task[1])
 
-        def clean_the_floor(args):
-            print("cleaning the floor with " + args)
-            pass
-
-        def __init__(self, pool):
-            objects = (self, pool)
-            thread = Thread(target = self.work, args = objects)
-            task = self.clean_the_floor
-            args = ()
+        def __init__(self, pool, cond):
+            self.should_work = True
+            self.thread = Thread(target = self.__work__, args = (pool, cond))
+            self.thread.start()
 
 
 def task(args):
     print("working" + str(args[1]) + " " + str(args[2]))
+    time.sleep(args[1])
+
 pool = ThreadPool
 pool.initial_setup(pool, 12)
+for i in range(0, 24):
+    pool.do(pool, task, (i, i + 25))
